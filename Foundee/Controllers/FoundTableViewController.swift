6755f9f9fd28b7635fccbc7717e8122b3924cfb4
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-12-22 19:43:16 +0000

    Create storybord for the lost and found views.

---
+++ Foundee/Controllers/FoundTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FoundViewController.swift
+//  Foundee
+//
+//  Created by Nedyalko Draganov on 22/12/2024.
+//
+
+import UIKit
+
+class FoundTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Foundee/Controllers/LostTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LostViewController.swift
+//  Foundee
+//
+//  Created by Nedyalko Draganov on 22/12/2024.
+//
+
+import UIKit
+
+class LostTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
