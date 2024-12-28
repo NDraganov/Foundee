d32484efca7e82dba72d6f50a61b8e52a3613e0b
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-12-28 23:01:51 +0000

    Filter the items data and display the found items in the Found table view.

--- Foundee/Controllers/FoundTableViewController.swift
+++ Foundee/Controllers/FoundTableViewController.swift
@@ -6,12 +6,52 @@
 //
 
 import UIKit
+import RealmSwift
 
 class FoundTableViewController: UITableViewController {
+    
+    private let realm = try! Realm()
+    private var foundItems: Results<Item>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadFoundItems()
+        
+        // Register the custom table view cell
+        tableView.register(LostItemTableViewCell.nib(), forCellReuseIdentifier: LostItemTableViewCell.identifier)
+    }
+    
+//MARK: - TableView Datasource Methods
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return foundItems?.count ?? 1
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let customCell = tableView.dequeueReusableCell(withIdentifier: LostItemTableViewCell.identifier, for: indexPath) as! LostItemTableViewCell
+            
+            if let foundItem = foundItems?[indexPath.row] {
+                customCell.lostItemLabel?.text = foundItem.item
+                customCell.lostItemNameLabel?.text = foundItem.name
+                customCell.lostItemNoteLabel?.text = foundItem.note
+            } else {
+                customCell.lostItemLabel?.text = "No Found Items"
+            }
+            
+            return customCell
+        }
+        
+        override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 75
+        }
+    
+//MARK: - Model Manupulations Methods
+    
+    func loadFoundItems() {
+        let predicate = NSPredicate(format: "isReturned == true")
+        foundItems = realm.objects(Item.self).filter(predicate)
+        tableView.reloadData()
     }
 
 }
