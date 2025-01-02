6ad7d798a56568f2d07180bed477b14577c3a04e
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2025-01-02 02:48:06 +0000

    Create FoundItem Realm object.

---
+++ Foundee/Models/FoundItem.swift
@@ -0,0 +1,19 @@
+//
+//  FoundItem.swift
+//  Foundee
+//
+//  Created by Nedyalko Draganov on 02/01/2025.
+//
+
+import Foundation
+import RealmSwift
+
+class FoundItem: Object {
+    @objc dynamic var date: Date = Date()
+    @objc dynamic var note: String = ""
+    @objc dynamic var foundBy: String = ""
+    @objc dynamic var locationFound: String = ""
+    @objc dynamic var currentLocation: String = ""
+    @objc dynamic var image: NSData = NSData()
+    @objc dynamic var isReturned: Bool = false
+}
